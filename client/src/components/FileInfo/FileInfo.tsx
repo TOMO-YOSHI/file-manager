e27998d2a8b60838a73f4aad8a6f79cc14aa9911
TOMO-YOSHI <lucky.paddy10@gmail.com>
2021-06-03 23:54:57 -0700

    implement no image handling

--- client/src/components/FileInfo/FileInfo.tsx
+++ client/src/components/FileInfo/FileInfo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './FileInfo.module.scss';
 import { File } from '../../redux/files';
 
@@ -7,6 +7,17 @@ interface Props {
 };
 
 const FileInfo: React.FC<Props> = ({file}) => {
+    const [imgSrc, setImgSrc] = useState<string>();
+
+    useEffect(()=>{
+        if (file?.image_url) {
+            setImgSrc(file.image_url)
+        }
+    }, [file])
+
+    const onError = () => {
+        setImgSrc('error');
+    }
 
     if(!file) {
         return null;
@@ -14,7 +25,23 @@ const FileInfo: React.FC<Props> = ({file}) => {
 
     return (
         <>
-            <img src={file.image_url} />
+            {
+                imgSrc === 'error' ?
+                    <div className={styles.noImage}>
+                        <p className={styles.faildText}>
+                            Failed to load<br />Please refresh browser
+                        </p>
+                    </div>
+                    : imgSrc ?
+                        <img
+                            className={styles.fileImage}
+                            src={imgSrc}
+                            onError={onError}
+                        />
+                        :
+                        <div className={styles.noImage}>No Image</div>
+            }
+            {/* <img src={file.image_url} /> */}
             <p className={styles.fileName}><span className={styles.tag}>Name:</span> {file.file_name}</p>
             <p><span className={styles.tag}>Type:</span> {file.file_type}</p>
             <p><span className={styles.tag}>Size:</span> {file.file_size_kb} KB</p>
@@ -25,7 +52,11 @@ const FileInfo: React.FC<Props> = ({file}) => {
                 : '-'
             }</p>
             <p><span className={styles.tag}>Memo:</span> </p>
-            <p>{file.memo_text}</p>
+            {
+                file.memo_text ?
+                    <p>{file.memo_text}</p>
+                    : 'No memo'
+            }
         </>
     );
 };
